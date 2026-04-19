7d769ec1bb291d0b33b2478ae572022de867eefa
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-04-19 12:05:08 -0300

    feat(modelo): adiciona construtor para Pessoa

--- code/src/modelo/Pessoa.java
+++ code/src/modelo/Pessoa.java
@@ -4,6 +4,11 @@ public class Pessoa {
     private int codigo;
     private String nome;
 
+    public Pessoa(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
     public int getCodigo() {
         return this.codigo;
     }
