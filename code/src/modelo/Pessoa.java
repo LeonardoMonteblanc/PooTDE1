c2b558f65679819aa2d41c3b4a72339104b7df77
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-04-19 11:35:04 -0300

    feat(modelo): inclusao da classe abstrata Pessoa

--- code/src/modelo/Pessoa.java
+++ code/src/modelo/Pessoa.java
@@ -1,3 +1,29 @@
+package modelo;
+
 public class Pessoa {
-    
+    private int codigo;
+    private String nome;
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setCodigo(int codigo)  {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Pessoa codigo: %d\nNome: %s",codigo, nome)";
+    }
+
+
 }
