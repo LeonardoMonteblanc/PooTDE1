198882413259cb2cb74f720dd7046588fe8a478a
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-04-19 12:19:58 -0300

    feat(modelo): implementacao da classe fornecedor

--- code/src/modelo/Fornecedor.java
+++ code/src/modelo/Fornecedor.java
@@ -1,3 +1,26 @@
-public class Fornecedor {
+package modelo;
+
+public class Fornecedor extends Pessoa {
+    private String cnpj;
     
+    public Fornecedor(int codigo, String nome, String cnpj) {
+        super(codigo, nome);
+        this.cnpj = cnpj;
+    }
+
+// GETS ======================================================
+    public String getCnpj() {
+        return this.cnpj;
+    }
+
+// SETS ======================================================
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Fornecedor]: cnpj: %s",cnpj);
+    }
+
 }
