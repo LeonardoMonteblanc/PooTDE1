981c1c4b09c884ec99c5f3dd5eb19a45e3e6720e
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-04-19 11:44:25 -0300

    feat(modelo): adiciona classe Usuario

--- code/src/modelo/Usuario.java
+++ code/src/modelo/Usuario.java
@@ -1,3 +1,42 @@
+package modelo;
+
 public class Usuario {
+    private String login;
+    private String senha;
+    private String nivelAcesso;
+
+    public Usuario(String login, String senha, String acesso) {
+        this.login = login;
+        this.senha = senha;
+        this.nivelAcesso = acesso;
+    }
+// GETS ======================================================
+    public String getLogin() {
+        return this.login;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public String nivelAcesso() {
+        return this.nivelAcesso;
+    }
+// SETS ======================================================
+    public void setLogin(String login) {
+        this.login = login;
+    }
     
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setNivelAcesso(String acesso) {
+        this.nivelAcesso = acesso;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Usuario]: login: %s%nsenha:%s%nnivel acesso: %s", login, senha, nivelAcesso);
+    }
 }
