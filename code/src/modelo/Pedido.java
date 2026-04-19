734883ff9edf08966eef1b0beb69dd92319f38db
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-04-19 13:06:00 -0300

    feat(modelo): implementacao dos metodos da classe Pedido

--- code/src/modelo/Pedido.java
+++ code/src/modelo/Pedido.java
@@ -1,3 +1,44 @@
+package modelo;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Pedido {
+    private ArrayList<ItemPedido> itens;
     
+    public Pedido(){
+
+    }
+
+    public void adicionarItem(Produto p, int qtd) {
+        for(ItemPedido i : itens ) {
+            if(i.getProduto().equals(p)) {
+                i.setQuantidade(i.getQuantidade() + qtd);
+            } else {
+                itens.add(new ItemPedido(p, qtd));
+            }
+        }
+    }
+
+    public void removerItem(Produto p, int qtd) {
+        Iterator<ItemPedido> it = itens.iterator();
+        while(it.hasNext()) {
+            it.next().getProduto().equals(p);
+            it.remove();
+        }
+    }
+
+// GETS ======================================================
+
+    public ArrayList<ItemPedido> getItens() {
+        return itens;
+    }
+// SETS ======================================================
+    public void setItens(ArrayList<ItemPedido> itens) {
+        this.itens = itens;
+    }
+    
+    @Override 
+    public String toString() {
+        return String.format("[Pedido]: itens: %s",itens);
+    }
 }
