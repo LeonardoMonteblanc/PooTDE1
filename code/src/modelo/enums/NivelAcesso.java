a75821a15f0f3a4d5987a49762c55d332707c10c
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-06-20 21:41:46 -0300

    refactor(enums): ajusta constantes de permissoes e niveis de acesso

--- code/src/modelo/enums/NivelAcesso.java
+++ code/src/modelo/enums/NivelAcesso.java
@@ -4,16 +4,5 @@ import java.util.EnumSet;
 import java.util.Set;
 
 public enum NivelAcesso {
-    CLIENTE(EnumSet.of(Permissao.CONSULTAR, Permissao.FAZER_PEDIDO)),
-    ADMIN(EnumSet.of(Permissao.CONSULTAR, Permissao.CADASTRAR, Permissao.EXCLUIR,Permissao.ALTERAR));
-
-    private final Set<Permissao> permissoes;
-
-    NivelAcesso(Set<Permissao> permissoes) {
-        this.permissoes = permissoes;
-    }
-
-    public boolean temPermissao(Permissao p) {
-        return permissoes.contains(p);
-    }
+    ADMIN, CLIENTE;
 }
--- code/src/modelo/enums/Permissao.java
+++
@@ -1,5 +0,0 @@
-package modelo.enums;
-
-public enum Permissao {
-    CONSULTAR, CADASTRAR, EXCLUIR, ALTERAR, FAZER_PEDIDO;
-}
